ed2d5bea6cb0cc14081faf0dc539a73fb8548ade
Raczkowski <raczkowskir@yahoo.com>
2017-03-12 18:07:17 +0100

    first commit

---
+++ Skrypt1.ps1
@@ -0,0 +1,6 @@
+﻿$compname = "localhost"
+$file = "C:\Users\Rafał\Desktop\Procesy.txt"
+Write-Host "Zbieram dane..."
+Get-WmiObject -computername $compname -class win32_process |
+Select-Object name, processID, WorkingSetSize |
+Format-Table | Out-File $file
